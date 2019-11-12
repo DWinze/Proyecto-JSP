b8ab2102f82179c7fe5055177722ce856f2add78
David Winze <38311518+DWinze@users.noreply.github.com>
2019-11-12 13:06:07 +0100

    Add files via upload

---
+++ jsp proyecto/src/Humano.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public abstract class Humano {
+	private int vida;
+	 ArrayList<Movimiento> movimientos;
+	
+	
+	public Humano (int v) {
+		
+		this.vida=v;
+		movimientos = new ArrayList();
+	}
+
+
+	public int getVida() {
+		return vida;
+	}
+
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+
+	public ArrayList getMovimientos() {
+		return movimientos;
+	}
+
+
+	public void setMovimientos(ArrayList movimientos) {
+		this.movimientos = movimientos;
+	}
+
+	
+	
+	
+	
+	
+
+}
---
+++ jsp proyecto/src/Movimiento.java
@@ -0,0 +1,13 @@
+
+public class Movimiento {
+	private String nombre;
+	private int daño;
+	
+	public Movimiento(String n, int d) {
+		this.nombre=n;
+		this.daño=d;
+		
+		
+	}
+
+}
